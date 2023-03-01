597589142243e2aaad903bcf227e3f247824c17d
ann19853008 <ann19853008@mail.ru>
2023-03-01 11:29:08 +0300

    Cats Arraylist example

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import ru.synergy.Cat;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main {
@@ -12,9 +13,16 @@ public class Main {
       cats[1]=new Cat("Begemot");
       cats[2]=new Cat("Fillip");
 
-      //cats[1]=null;
+     // cats[1]=null;
 
         System.out.println(Arrays.toString(cats));
+
+        ArrayList<Cat> catsList = new ArrayList<>();
+         for (Cat cat:cats){
+             catsList.add(cat);
+         }
+           catsList.add( new Cat("Begemot2"));
+        System.out.println(catsList.toString());
 
 
 
