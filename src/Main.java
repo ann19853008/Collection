8804bf0365e8522cde06798df365c982edd5088e
ann19853008 <ann19853008@mail.ru>
2023-03-01 11:18:07 +0300

    Cats example

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import ru.synergy.Cat;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+
+
+      Cat[]  cats = new Cat[3];
+      cats[0]=new Cat("Tomas");
+      cats[1]=new Cat("Begemot");
+      cats[2]=new Cat("Fillip");
+
+      //cats[1]=null;
+
+        System.out.println(Arrays.toString(cats));
+
+
 
     }
 }
